9ac6ad877c9c76a891c999fa89cf7d41fd33ed67
Patrick Rebadow <prebs91@yahoo.com>
2026-04-02 23:30:21 -0400

    Limit build concurrency for Vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   env: {
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  }
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 }
 module.exports = nextConfig
